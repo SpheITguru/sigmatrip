9910da5b28de7b0a4593e9e4c1d1ba978378aebe
sphephelo <sphe.mlu@gmail.com>
2021-05-06 20:50:07 +0200

    testing mysqli

--- connection.php
+++ connection.php
@@ -1,14 +1,15 @@
 <?php 
 
 
-    $con = mysqli_connect('$_ENV["DB_HOST','$_ENV["DB_USER','$_ENV["DB_PASSWORD','sigmadb');
+    $con = mysqli_connect('$_ENV["DB_HOST]','$_ENV["DB_USER]','$_ENV["DB_PASSWORD]','sigmadb');
 
     if(!$con)
     {
 
         echo 'Please check your Database Connection';
+       
 
     }
-
+    echo $_ENV["DB_HOST];
 
 ?>
