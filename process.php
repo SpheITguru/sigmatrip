9e779a8b16326bee77d6d092362d453ddcc2f69e
sphephelo <sphe.mlu@gmail.com>
2021-05-06 19:16:44 +0200

    testing inputs

--- process.php
+++ process.php
@@ -1,9 +1,15 @@
 <?php
 
+    require_once('connection.php');
 
     if(isset($_POST['submit']))
     {
-        echo ' Working ';
+        $username = mysqli_real_escape_string($_POST['username'], $con);
+        $email = mysqli_real_escape_string($_POST['email'], $con);
+        $password = mysqli_real_escape_string($_POST['password'], $con);
+        $cpassword = mysqli_real_escape_string($_POST['cpassword'], $con);
+
+        echo $username, $email, $password, $cpassword;
     }
 
 ?>
