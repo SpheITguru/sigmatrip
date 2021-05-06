132b6bde997ada5af46cc11d01d7e4a54f6f1e6f
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-05-06 22:58:23 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,3 +1,36 @@
+<?php
+
+    require_once('connection.php');
+
+    if(isset($_POST['submit']))
+    {
+        $username = mysqli_real_escape_string($con, $_POST['username']);
+        $email = mysqli_real_escape_string($con, $_POST['email']);
+        $password = mysqli_real_escape_string($con, $_POST['password']);
+        $cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
+
+        if($password!=$cpassword)
+        {
+              echo 'Password Not Matching';
+        }else 
+        {
+            $pass=md5($password);
+            $sql = "insert into users (username, email, password) values ('$username', '$email', '$pass')";
+            $result = mysqli_query($con, $sql);
+            
+            if($result)
+            {
+                echo 'Your Record has been saved in the Database';
+            }
+            else 
+            {
+                echo 'Check your inputs';
+            }
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
