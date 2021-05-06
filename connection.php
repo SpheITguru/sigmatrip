2723621ed005970e075078f6bba08e358cb0c7ff
sphephelo <sphe.mlu@gmail.com>
2021-05-06 20:57:24 +0200

    env variable

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-    $con = mysqli_connect($_ENV["DB_HOST"],$_ENV["DB_USER"],$_ENV["DB_PASSWORD"],'sigmadb');
+    $con = mysqli_connect(getenv("DB_HOST"),getenv("DB_USER"),getenv("DB_PASSWORD"),'sigmadb');
 
     if(!$con)
     {
@@ -10,6 +10,6 @@
        
 
     }
-    echo $_ENV["DB_HOST"];
+    echo getenv("DB_HOST");
 
 ?>
