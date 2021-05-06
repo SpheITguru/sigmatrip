9c1df91c0d32e2de3558d558794b046d233c3719
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-05-06 23:37:19 +0200

    Update process2.php

--- process2.php
+++ process2.php
@@ -1,6 +1,42 @@
 <?php
 
     echo 'Starting the page';
-    
+    require_once('connection.php');  
+
+    if(isset($_POST['submit']))
+    {
+        echo 'Button was clicked';
+        
+        $username = $_POST['username']);
+        $password = $_POST['password']);
+
+        if(empty($username) || empty($password))
+        {
+              echo 'Please fill in the blanks';
+        }else 
+        {
+            echo 'Quering the DB';
+            $query = "select * from users where username = '$username'";
+            $result = mysqli_query($con, $query);
+            
+            if($row=mysqli_fetch_assoc($result))
+            {
+                $db_password = $row['password'];
+                
+                if(md5($password) == $db_password)
+                {
+                    header("location:dashboard.html");               
+                }
+                else
+                {
+                    echo 'Incorrect Password';
+                }
+            }
+            else 
+            {
+                echo 'Check your inputs';
+            }
+        }
+    }
 
 ?>
