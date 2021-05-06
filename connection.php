f07eb349b24ecc8b63f60c22867f69b6c1df4e9f
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-05-06 22:04:51 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,15 +1,10 @@
 <?php 
 
-
     $con = mysqli_connect(getenv("DB_HOST"),getenv("DB_USER"),getenv("DB_PASSWORD"),'sigmadb');
 
     if(!$con)
     {
-
         echo 'Please check your Database Connection';
-       
-
     }
-    echo getenv("DB_HOST");
 
 ?>
