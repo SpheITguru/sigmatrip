475483c1defdfc20ebdd74267dc53614d86a6466
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-05-06 23:34:13 +0200

    Update process2.php

--- process2.php
+++ process2.php
@@ -1,16 +1,6 @@
 <?php
 
     echo 'Starting the page';
-    require_once('connection.php');  
-
-    if(isset($_POST['submit']))
-    {
-        echo 'Button was clicked';
-        
-        $username = $_POST['username']);
-        $password = $_POST['password']);
-
-        
-    }
+    
 
 ?>
